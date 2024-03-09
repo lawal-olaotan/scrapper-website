328a4dfb4ce4660b909b10ddcfda00713b0f8983
Olaotan Lawal <olaotan104@gmail.com>
2024-03-09 17:32:39 +0000

    inital files

---
+++ index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+    const formElement = document.querySelector('#scrappeForm')
+    const url = document.querySelector('#url')
+    const name = document.querySelector("#name")
+
+    formElement.addEventListener('submit', async(event)=> {
+        event.preventDefault()
+        const webpage =  await getPageContent(url.value)
+        const data = JSON.stringify({webpage, name:name.value})
+        const backendURI = "http://scrapper-766800069.eu-west-2.elb.amazonaws.com:8080/"
+        const response = await fetch(backendURI+'save',{
+            method: 'POST',
+            body:data,  
+            headers:{"Content-Type": "application/json" }
+        })
+        
+        if(response.ok) return clearInput('success')
+
+        if(!response.ok){
+            return clearInput('already exist')
+        } 
+        
+    })
+
+
+    const getPageContent = async(url)=> {
+            const response = await fetch(url)
+            if(!response.ok) return 'unable to fetch';
+            const textContent = await response.text(); 
+            return textContent
+    }
+
+    const clearInput = (message) => {
+        alert(message)
+        url.value = ''
+        name.value = ''
+    }
+
+
+})
